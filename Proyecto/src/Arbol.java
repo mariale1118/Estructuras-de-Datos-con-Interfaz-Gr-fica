c5fd133db6d9cca7f294266947bf6d9fcdff022d
SebasChaves6 <sebaschaves2901@gmail.com>
2024-11-26 03:05:29 -0600

    metodos de busqueda y correcion de insercion

--- Proyecto/src/Arbol.java
+++ Proyecto/src/Arbol.java
@@ -1,44 +1,67 @@
 public class Arbol {
     
     //metodo para insertar de manera recursiva
-    public void insertar(Nodo raiz, int valor) {
-        if (raiz == null) {
-            raiz = new Nodo(valor);
+    public Nodo insertar(Nodo nodo, int dato) {
+        //caso base
+        if (nodo == null) {
+            nodo = new Nodo(dato);
+        }
+        //evita duplicados
+        if (dato == nodo.dato) {
+            return nodo;
+        }
+        //tipico movimiento de insercion
+        if (dato < nodo.dato) {
+            nodo.anterior = insertar(nodo.anterior, dato);
+        } else{
+            nodo.siguiente = insertar(nodo.siguiente, dato);
+        }
+
+        return nodo;
+        
+    }
+    //metodo recursivo para buscar
+    public Nodo buscar(Nodo nodo, int dato) {
+        if (nodo == null) {
+            return null;
+        }
+        //caso base encontro el dato
+        if (dato == nodo.dato) {
+            return nodo;
+        }
+        //tipico movimiento de busqueda
+        if (dato < nodo.dato) {
+            return buscar(nodo.anterior, dato);
         } else {
-            if (valor < raiz.dato) {
-                if (raiz.anterior == null) {
-                    raiz.anterior = new Nodo(valor);
-                } else {
-                    insertar(raiz.anterior, valor);
-                }
-            } else {
-                if (raiz.siguiente == null) {
-                    raiz.siguiente = new Nodo(valor);
-                } else {
-                    insertar(raiz.siguiente, valor);
-                }
-            }
+            return buscar(nodo.siguiente, dato);
         }
     }
+
+
+    public Nodo eliminarNodo(Nodo nodo, int dato) {
+
+    }
+
+    
     //metodo sencillo de impresion en orden (de izquierda a derecha)
-    public void imprimir(Nodo raiz) {
-        if (raiz != null) {
-            imprimir(raiz.anterior);
-            System.out.println(raiz.dato);
-            imprimir(raiz.siguiente);
+    public void imprimir(Nodo nodo) {
+        if (nodo != null) {
+            imprimir(nodo.anterior);
+            System.out.println(nodo.dato);
+            imprimir(nodo.siguiente);
         }
     }
 
     public static void main(String[] args) {
-        Nodo raiz = new Nodo(3);
+        Nodo nodo = new Nodo(3);
         Arbol arbol = new Arbol();
-        arbol.insertar(raiz, 1);
-        arbol.insertar(raiz, 6);
-        arbol.insertar(raiz, 2);
-        arbol.insertar(raiz, 5);
-        arbol.insertar(raiz, 4);
+        arbol.insertar(nodo, 1);
+        arbol.insertar(nodo, 6);
+        arbol.insertar(nodo, 2);
+        arbol.insertar(nodo, 5);
+        arbol.insertar(nodo, 4);
 
-        arbol.imprimir(raiz);
+        arbol.imprimir(nodo);
 
 
     }
