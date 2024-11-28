614d8ce267eec70a082dde70271ec84ebcb87124
Mariale <107595778+mariale1118@users.noreply.github.com>
2024-11-28 11:26:11 -0600

    Update ClasePrincipal.java

--- ColaProyecto/ClasePrincipal.java
+++ ColaProyecto/ClasePrincipal.java
@@ -1,33 +1,43 @@
-
-/**
- * Write a description of class ClasePrincipal here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import javax.swing.JOptionPane;
 public class ClasePrincipal
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class ClasePrincipal
-     */
-    public ClasePrincipal()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main (String [] args) {
+        int opcion = 0, nodoInformacion = 0;
+        Cola cola = new Cola();
+        
+        do {
+            try {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null, 
+                 "Menu de opciones/n/n" + "1 Insertar nodo/n" + "2 Extraer nodo/n" +
+                 "3 Mostrar contenido cola/n" + "4 salir/n/n"));
+                
+                 switch(opcion) {
+                     case 1 :
+                         nodoInformacion = Integer.parseInt(JOptionPane.showInputDialog( null, 
+                         "Ingrese el valor a guardar en el nodo"));
+                         cola.Insertar(nodoInformacion);
+                         break;
+                    case 2 :if (!cola.colaVacia()) {
+                                JOptionPane.showMessageDialog(null, "Se extrajo el nodo de valor: " + 
+                             cola.Extraer());
+                            } else {
+                                JOptionPane.showMessageDialog(null, "La cola está vacía");}
+                        
+                         break;
+                    case 3 :
+                        cola.MostrarContenido();
+                         break;
+                    case 4: 
+                        opcion = 4;
+                         break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "no disponible");
+                        break;
+                         
+                    }
+            } catch (NumberFormatException e) {
+            }
+        } while (opcion  != 4);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
